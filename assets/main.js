b5473601e93dc471a4fa5fe19164d7d19e480a68
strong-code <colin@strongco.de>
2020-06-02 22:52:51 -0700

    no need for a framework, keep this simple

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+function init() {
+  console.log('js loaded!')
+  initPaste()
+}
+
+function initPaste() {
+  const c = $('.container')
+  c.pastableNonInputable()
+  
+  c.on('pasteImage', (ev, data) => {
+    console.log(data.blob)
+  })
+  .on('pasteText', (ev, data) => {
+    console.log(data.text)
+  })
+}
