b543110b644e931f42eebc6408babbb76d3b1e38
strong-code <colin@strongco.de>
2020-06-04 00:50:19 -0700

    start link menu in json file. Add rendering of link menu in js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 function init() {
   initPaste()
+  initLinks()
 }
 
 function initPaste() {
@@ -11,5 +12,19 @@ function initPaste() {
   })
   .on('pasteText', (ev, data) => {
     console.log(data.text)
+  })
+}
+
+function initLinks() {
+  fetch('assets/links.json')
+  .then(res => res.json())
+  .then(json => {
+    json.categories.forEach(c => {
+      let category = $('#' + c.title)
+      c.links.forEach(entry => {
+        let str = `<a href="${entry.url}">${entry.title}</a>`
+        category.append(str)
+      })
+    })
   })
 }
