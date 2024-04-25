569c84cba62e44e6e9847bd8653fddd40dd6399d
strong-code <colin@strongco.de>
2024-04-24 22:45:25 -0700

    Add tracking support

--- dist/assets/js/search.js
+++ dist/assets/js/search.js
@@ -1,3 +1,5 @@
+import './jquery-3.5.1.min.js'
+
 const searchSources = {
   "!g":        ["https://www.google.com/search?&q={Q}",                   "Google"],
   "!b":        ["https://search.brave.com/search?q={Q}",                  "Brave"],
@@ -12,7 +14,24 @@ const searchSources = {
   "!last":     ["https://www.last.fm/search?q={Q}",                       "Last.fm"],
   "!r":        ["https://duckduckgo.com/?q=reddit+{Q}",                   "Reddit"],
   "!a":        ["https://smile.amazon.com/s?k={Q}",                       "Amazon"],
-  "$":         ["https://finance.yahoo.com/quote/{Q}",                    "Yahoo Finance"]
+  "$":         ["https://finance.yahoo.com/quote/{Q}",                    "Yahoo Finance"],
+  "!t":        ["https://api.goshippo.com/tracks/",                       "Tracking"]
+}
+
+function handleTrack(carrier, trackingNumber) {
+  $.ajax({
+    url: `https://strongco.de/api/track/new`,
+    type: 'POST',
+    data: {tracking_number: trackingNumber, carrier: carrier},
+    success: function(data) {
+      $('#welcomeMsg').text(`Shipment ${trackingNumber} added to shipment tracking`)
+    },
+    error: function(err) { 
+      $('#welcomeMsg').text(`Error creating Shippo tracking entry`)
+    }
+  })
+
+  return
 }
 
 function initSearch() {
@@ -20,12 +39,14 @@ function initSearch() {
   const welcomeMsg = $('#welcomeMsg')
   searchBar.focus()
   let source = searchSources['!b'] // default
+  let chunk
 
   searchBar.attr('placeholder', source[1])
 
   searchBar.on('keyup', e => {
     if (e.key === ' ') {
-      let chunk = searchBar.val().split(' ')[0]
+      chunk = searchBar.val().split(' ')[0]
+
       if (searchSources[chunk]) {
         source = searchSources[chunk]
         welcomeMsg.text(`searching with ${source[1].toLowerCase()}`)
@@ -34,6 +55,11 @@ function initSearch() {
     }
 
     if (e.key === 'Enter') {
+      if (chunk === '!t') {
+        const [carrier, trackingNumber] = searchBar.val().split(' ')
+        return handleTrack(carrier, trackingNumber)
+      }
+
       window.location = source[0].replace("{Q}", encodeURIComponent(searchBar.val().trim()))
     }
   })
